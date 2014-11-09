eddd4da5434832baa7b872e879fbc72b4fdf5941
faisaldeveloper <faisal.shahzad786@gmail.com>
2014-11-09 10:57:55 +0500

    third change ignoring config main.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 $yii=dirname(__FILE__).'/../../yiiframework1.1.12/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
+
+//test comment
+
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
